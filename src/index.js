d9ac2028fa3ea4e6a737c2540b9306f3b0a2f69b
Rafael Mendes <faelmob@gmail.com>
2023-09-26 09:06:30 -0300

    Alteração em função

--- src/index.js
+++ src/index.js
@@ -24,6 +24,18 @@ function verifyIfExistsAccountCPF(request, response, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce(
+    (acc, operation) =>
+      operation.type === "credit"
+        ? acc + operation.amount
+        : acc - operation.amount,
+    0
+  );
+
+  return balance;
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -66,6 +78,17 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
   customer.statement.push(statementOperation);
 
   return response.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: "Insuficient funds!" });
+  }
 });
 
 app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
